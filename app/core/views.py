a57c80ec6a60db16bf229f37c96890c9425e2836
himanmenGit <study.himanmen@gmail.com>
2019-01-23 18:58:36 +0900

    simpleisbetterthancomplex 사이트 파일업로드 튜토리얼2 추가

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.core.files.storage import FileSystemStorage
+
+from .models import Book
+from .forms import BookForm
 
 
 class Home(TemplateView):
@@ -15,3 +18,23 @@ def upload(request):
         name = fs.save(upload_file.name, upload_file)
         context['url'] = fs.url(name)
     return render(request, 'core/upload.html', context)
+
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'core/book_list.html', {
+        'books': books
+    })
+
+
+def upload_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+        return render(request, 'core/upload_book.html', {
+            'form': form
+        })
